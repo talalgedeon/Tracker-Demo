0bbf145be71042c55e00b351492f1b7588a3b737
Talal A Gedeon <talalagedeon@Talals-MacBook-Pro.local>
2020-09-30 11:45:54 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,127 +1,103 @@
 #include "Particle.h"
-
 #include "tracker_config.h"
 #include "tracker.h"
-
 #include "Air_Quality_Sensor.h"
 #include "Adafruit_BME280.h"
 #define AQS_PIN A2
 
-
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
-
-PRODUCT_ID(TRACKER_PRODUCT_ID);
-PRODUCT_VERSION(TRACKER_PRODUCT_VERSION);
+PRODUCT_ID(TRACKER_PRODUCT_ID); 
+PRODUCT_VERSION(TRACKER_PRODUCT_VERSION); 
 
 SerialLogHandler logHandler(115200, LOG_LEVEL_TRACE, {
     { "app.gps.nmea", LOG_LEVEL_INFO },
     { "app.gps.ubx",  LOG_LEVEL_INFO },
     { "ncp.at", LOG_LEVEL_INFO },
     { "net.ppp.client", LOG_LEVEL_INFO },
-});
+}); 
 
-// Library: Grove_Temperature_And_Humidity_Sensor
-AirQualitySensor aqSensor(AQS_PIN);
-Adafruit_BME280 bme;
+AirQualitySensor aqSensor(AQS_PIN); 
+Adafruit_BME280 bme; 
 
-
-// Sample the temperature sensor every 2 seconds. This is done so the outlier values can be filtered out easily.
 const unsigned long CHECK_PERIOD_MS = 2000;
 unsigned long lastCheck = 0;
-int useCaseState = 0; // useCase 1 = Cold Chain(temp/humid), useCase 2 (AQM) = , useCase 3 = (pressure)
+int useCaseState(int input); // useCase 1 = Cold Chain(temp/humid), useCase 2 (AQM) = , useCase 3 = (pressure) 
 
 
-
-void locationGenerationCallback(JSONWriter &writer, LocationPoint &point, const void *context); // Forward declaration
+void locationGenerationCallback(JSONWriter &writer, LocationPoint &point, const void *context); // Forward declaration :)
 
 
 void setup()
 {
-    Tracker::instance().init();
+    Tracker::instance().init(); 
 
     aqSensor.init();
+
     bme.begin();
 
-    Particle.function(thisUseCase);
+    Particle.function("Switch Case", useCaseState);
 
-    // Callback to add key press information to the location publish
     Tracker::instance().location.regLocGenCallback(locationGenerationCallback);
 
-    Particle.connect();
+    Particle.connect(); 
 }
+
 
 void loop()
 {
-    Tracker::instance().loop();
+    Tracker::instance().loop(); 
 
     if (millis() - lastCheck >= CHECK_PERIOD_MS) {
-        lastCheck = millis();
+
+        // Should i do anything here?
+        lastCheck = millis(); 
+
     }
 }
-
-
-thisUseCase (String input){
-    if input = "Cold Chain"
-        useCaseState = 1;
-    
-    if input = "heavyTrucking"
-        useCaseState = 2;
-    
-    if input = "railCar"
-        useCaseState = 3;
-    
-}
-
-
-
-// put in some function and let it redirect 
 
 void locationGenerationCallback(JSONWriter &writer, LocationPoint &point, const void *context)
 {
-    callback (input){
+    callback (input){ // not sure i need this
         parseFunction();
     }
-
+    
 }
-
-parseFunction (input){
-    switch (useCaseState){
-        case 1: coldChain
+// Should void be added here? is the switch case right, when 1 is called in function useCaseState the device will adpot coldChian method
+void parseFunction (input){
+    switch (useCaseState){ 
+        case 1: coldChain() 
         break;
 
-        case 2: railCar
+        case 2: railCar() 
         break;
 
-        case 3: heavyTrucking
+        case 3: heavyTrucking()
         break;
     }
 }
 
-coldChain (){
-    // Read Measurments from BME 280
+
+
+
+void coldChain (){
     int temp = (int)bme.readTemperature();
     int humidity = (int)bme.readHumidity();
 
-    // Add Names under device page
-    writer.name("Temperature").value(temp, 1);
+    writer.name("Temperature").value(temp, 1); 
     writer.name("Humidity").value(humidity, 1);
 }
 
-railCar (){
-    // Read Measurments from BME 280
+void railCar (){
     int pressure = (int)(bme.readPressure() / 100.0F);
     int humidity = (int)bme.readHumidity();
 
-    // Add Names under device page
     writer.name("Pressure").value(pressure, 1);
     writer.name("Humidity").value(humidity, 1);
-=
 }
 
-heavyTrucking(){
-    // Read Measurments from Air Quality v1.2
-    // Persona 2
+void heavyTrucking(){
+
     int quality = aqSensor.slope();
     String qual = "None";
 
@@ -142,8 +118,6 @@ heavyTrucking(){
         qual = "Fresh Air";
     }
 
-    // Write AQ under Device Page
+    // Where do i add writer, does it only work in location call back?
     writer.name("Quality").value(qual, 15); 
-
 }
-
